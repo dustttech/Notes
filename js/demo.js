02e62b39d0ee593322573e85e69d229df97b73b4
Dusttt <lucchanphong1997@gmail.com>
2021-02-21 09:38:07 +0700

    add input validation

--- js/demo.js
+++ js/demo.js
@@ -24,83 +24,12 @@ document.addEventListener('DOMContentLoaded',function(){
         close_btn.addEventListener('animationend', close_search, true);
         close_btn.addEventListener('webkitAnimationEnd', close_search, true);
     })
-    // END SEARCH
-    // NOTE 
-    // notes.forEach(note => {
-
-    // });
-    // END NOTE
 
 
 
 
-    const addNoteForm = document.querySelector('.add-note-overlay');
-
-    // SHOW ADD NOTE SECTION
-    const addNote = document.querySelector('#add-note');
-    addNote.addEventListener('click', function () {
-        addNoteForm.classList.add('show_overlay');
-    })
-
-    // CLOSE ADD NOTE
-    const closeAddNote = document.querySelector('#close_add_form');
-
-    closeAddNote.addEventListener('click',function() {
-        addNoteForm.classList.remove('show_overlay');
-    })
-
-    //EDIT NOTE SECTION
-    const editNoteForm = document.querySelector('.edit-note-overlay');
-    // SHOW EDIT NOTE
-    edit.addEventListener('click', function () {
-        editNoteForm.classList.add('show_overlay');
-
-        var key = document.querySelector('li.selected').getAttribute('id');
-        addFormEdit(key);
-    })
-    const editNote = document.querySelector('#add_edit_form');
-    editNote.addEventListener('click', function () {
-        updateEditNote();
-    })
 
 
-    function addFormEdit(key) {
-        // var selected = document.querySelector('li.selected');
-        // var title = document.querySelector('li h1').innerHTML;
-        // var content = document.querySelector('li p').innerHTML;
-        // var time = document.querySelector('li span').innerHTML;
-        // var tag = document.querySelector('li a').innerHTML;
-        var note = JSON.parse(localStorage[key]);
-
-        const noteTitle = document.querySelector('#edit-note-title');
-        const noteContent = document.querySelector('#edit-note-content');
-        const noteTag = document.querySelector('#edit-note-tag');
-        
-        noteTitle.value = note.title;
-        noteContent.value = note.content;
-        noteTag.value = note.tag;
-    }
-    function updateEditNote() {
-        var key = document.querySelector('li.selected').getAttribute('id');
-        var note = JSON.parse(localStorage[key]);
-
-        const noteTitle = document.querySelector('#edit-note-title').value;
-        const noteContent = document.querySelector('#edit-note-content').value;
-        const noteTag = document.querySelector('#edit-note-tag').value;
-        var dateValue = getCurrentTime();
-
-        note.title = noteTitle;
-        note.content = noteContent;
-        note.date = dateValue + " (edited)";
-        note.tag = noteTag;
-
-        localStorage.setItem(key, JSON.stringify(note));
-    }
-    // CLOSE EDIT NOTE
-    const closeEditNote = document.querySelector('#close_edit_form');
-    closeEditNote.addEventListener('click',function() {
-        editNoteForm.classList.remove('show_overlay');
-    })
     // ADD NOTE LOGIC
 
     // INITIAL
@@ -108,13 +37,78 @@ document.addEventListener('DOMContentLoaded',function(){
         var displayMsg = document.querySelector('.message > span');
         if (window["localStorage"]) {
 
+
+            // SHOW ADD NOTE SECTION
+
+            const showAddOverLay = document.querySelector('.add-note-overlay');
+
+            const addNote = document.querySelector('#add-note');
+            addNote.addEventListener('click', function () {
+                showAddOverLay.classList.add('show_overlay');
+            })
             // BTN ADD NOTE FORM
             const addFormNote = document.querySelector('#add_form');
+            const closeAddNote = document.querySelector('#close_add_form');
+            const inputField = document.querySelectorAll('.input-field');
+            inputField.forEach(input => {
+                input.addEventListener('invalid', function name(e) {
+                    e.preventDefault();
+                })
+            });
+            addFormNote.addEventListener('click', function () {
+                inputField.forEach(input => {
+                    input.setAttribute('placeholder', 'Please enter this')
+                });
+            });
+                // CLOSE ADD NOTE
+            closeAddNote.addEventListener('click',function() {
+                showAddOverLay.classList.remove('show_overlay');
+                inputField.forEach(input => {
+                    input.removeAttribute('placeholder')
+                });
+            })
+            // END ADD NOTE
 
-            addFormNote.onclick = createNote;
+            //EDIT NOTE SECTION
+
+            // SHOW EDIT NOTE SECTION
+            const editOverLay = document.querySelector('.edit-note-overlay');
+            // SHOW EDIT NOTE
+            edit.addEventListener('click', function () {
+                editOverLay.classList.add('show_overlay');
+
+                var key = document.querySelector('li.selected').getAttribute('id');
+                addFormEdit(key);
+            })
+            const editNote = document.querySelector('#add_edit_form');
+            editNote.addEventListener('click', function () {
+                inputField.forEach(input => {
+                    input.setAttribute('placeholder', 'Please enter this')
+                });
+            })
+            // CLOSE EDIT NOTE
+            const closeEditNote = document.querySelector('#close_edit_form');
+            closeEditNote.addEventListener('click',function() {
+                editOverLay.classList.remove('show_overlay');
+            })
+            // END EDIT NOTE
+
+
             // BTN DELETE NOTE
             del.onclick = removeNoteFromDOM;
 
+            // CHECK FORM VALID
+            const addForm = document.querySelector('#add-form');
+            addForm.addEventListener('submit', function () {
+                createNote();    
+            })
+            const editForm = document.querySelector('#edit-note');
+            editForm.addEventListener('submit', function () {
+                updateEditNote();
+            })
+
+    
+        
             var notesArray = getNotesArray();
             for (let i = 0; i < notesArray.length; i++) {
                 var key = notesArray[i];
@@ -213,7 +207,6 @@ document.addEventListener('DOMContentLoaded',function(){
     }
 
 
-
     function getNotesArray() {
         var notesArray = localStorage["notesArray"];
 
@@ -235,6 +228,8 @@ document.addEventListener('DOMContentLoaded',function(){
         var titleValue = noteTitle.value;
         var contentValue = noteContent.value;
         var tagValue = noteTag.value;
+
+
         var dateValue = getCurrentTime();
         var noteObj = {
             "title": titleValue,
@@ -272,7 +267,39 @@ document.addEventListener('DOMContentLoaded',function(){
         del.classList.toggle('btn-active');
     }
 
+    // EDIT NOTE  FORM 
+    function addFormEdit(key) {
+        // var selected = document.querySelector('li.selected');
+        // var title = document.querySelector('li h1').innerHTML;
+        // var content = document.querySelector('li p').innerHTML;
+        // var time = document.querySelector('li span').innerHTML;
+        // var tag = document.querySelector('li a').innerHTML;
+        var note = JSON.parse(localStorage[key]);
 
+        const noteTitle = document.querySelector('#edit-note-title');
+        const noteContent = document.querySelector('#edit-note-content');
+        const noteTag = document.querySelector('#edit-note-tag');
+        
+        noteTitle.value = note.title;
+        noteContent.value = note.content;
+        noteTag.value = note.tag;
+    }
+    function updateEditNote() {
+        var key = document.querySelector('li.selected').getAttribute('id');
+        var note = JSON.parse(localStorage[key]);
+
+        const noteTitle = document.querySelector('#edit-note-title').value;
+        const noteContent = document.querySelector('#edit-note-content').value;
+        const noteTag = document.querySelector('#edit-note-tag').value;
+        var dateValue = getCurrentTime();
+
+        note.title = noteTitle;
+        note.content = noteContent;
+        note.date = dateValue + " (edited)";
+        note.tag = noteTag;
+
+        localStorage.setItem(key, JSON.stringify(note));
+    }
 
 
 },false)
